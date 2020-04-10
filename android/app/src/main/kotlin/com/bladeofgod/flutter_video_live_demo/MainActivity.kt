97877a0b989b6d3d2ef29fded13141497176b8b1
bladeofgods <bladeofgods@163.com>
2020-04-10 16:16:26 +0800

    1.0

---
+++ android/app/src/main/kotlin/com/bladeofgod/flutter_video_live_demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bladeofgod.flutter_video_live_demo
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
